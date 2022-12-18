e2b92496da688051c9645f4ecc67c9f4a1a528e9
sammyshah <muzammil.shah@gmail.com>
2022-12-18 17:35:42 -0600

    terraform

--- ex2/instance/instance.tf
+++ ex2/instance/instance.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "web" {
   ami           = "ami-070d3c2a310535689"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [var.SGID]
 
   tags = {
     Name = "terraform"
   }
 }
+
+vaiable "SGID" {}
