54ecbb37a3fadc8df7d1aa1c01692b5235411046
sammyshah <muzammil.shah@gmail.com>
2022-12-19 09:02:47 -0600

    terraform

---
+++ 04-loops/approach01/main.tf
@@ -0,0 +1,15 @@
+variables "no-of-instaces" {
+  default = 2
+}
+
+resource "aws_instance" "web" {
+  count         = var.no-of-instances
+  ami           = "ami-070d3c2a310535689"
+  instance_type = "t3.micro"
+
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
