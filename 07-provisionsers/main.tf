d8f1ca68b5055c511ae2094de04b95097f467d4d
sammyshah <muzammil.shah@gmail.com>
2022-12-21 09:47:05 -0600

    terraform

--- 07-provisionsers/main.tf
+++ 07-provisionsers/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-070d3c2a310535689"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh]
 
   tags = {
     Name = "terraform"
